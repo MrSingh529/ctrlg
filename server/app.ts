2ee99ce2453978e50aeecceeee46d121376b8656
MrSingh529 <harpinder.singh@rvsolutions.in>
2026-01-14 17:25:27 +0530

    Fix vercel deployment config

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,11 @@
-import "dotenv/config";
 import express from "express";
 import { registerRoutes } from "./routes";
 
-export const app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-await registerRoutes(undefined as any, app);
+registerRoutes(null as any, app);
 
 export default app;
