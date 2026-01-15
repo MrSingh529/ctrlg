951bfc8b17487877bb353008740fb4f156054da4
MrSingh529 <harpinder.singh@rvsolutions.in>
2026-01-15 12:26:01 +0530

    Fix serverless entrypoint

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,11 @@
-import app from "./app.js";
+import express from "express";
+import { registerRoutes } from "../server/routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+await registerRoutes(null as any, app);
 
 export default app;
