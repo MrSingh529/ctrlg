2d55d2ae276279ad6979fdba924170b0a4ee02c7
MrSingh529 <harpinder.singh@rvsolutions.in>
2026-01-15 13:35:40 +0530

    Created Dist folder

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import { registerRoutes } from "../server/routes";
+import { registerRoutes } from "../server/routes.js";
 
 const app = express();
 
@@ -9,6 +9,5 @@ app.use(express.urlencoded({ extended: false }));
 
 // Register API routes ONLY
 registerRoutes(null as any, app);
-
 
 export default app;
